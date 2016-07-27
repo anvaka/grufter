b7920b133273219beed51ccec5c261c044e05fa5
anvaka <anvaka@gmail.com>
2016-07-26 23:21:04 -0700

    Initial project layout

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+  <graph-viewer></graph-viewer>
+  </div>
+</template>
+
+<script>
+import GraphViewer from './components/GraphViewer'
+
+export default {
+  components: {
+    GraphViewer
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  position: absolute;
+  background: black;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  color: #efefef;
+  font-family: Roboto,sans-serif;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+</style>
